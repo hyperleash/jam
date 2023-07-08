5a499cbbb75c85bf6b062a00adfee2c6210864ed
Celezt <59172226+Celezt@users.noreply.github.com>
2023-07-08 16:57:13 +0200

    Added MaxHealth

--- Assets/Scripts/AI/HealthBehaviour.cs
+++ Assets/Scripts/AI/HealthBehaviour.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 
 public class HealthBehaviour : MonoBehaviour
 {
+    public int MaxHealth
+    {
+        get => _maxHealth;
+        set
+        {
+            value = Mathf.Max(value, 0);
+
+            if (_maxHealth == value) 
+                return;
+
+            OnMaxHealthChangedCallback((value, value - _maxHealth));
+
+            if (_maxHealth < _health)
+                _health = value;
+
+            if (value <= 0)
+                OnDeathCallback();
+
+            _maxHealth = value;
+        }
+    }
+
     public int Health
     {
         get => _health;
         set
         {
-            value = Mathf.Max(value, 0);  // Cannot be negative.
+            value = Mathf.Clamp(value, 0, _maxHealth);  // Cannot be negative or greather han max health.
 
             if (_health == value)  // Health is unaffected.
                 return;
@@ -25,7 +47,19 @@ public class HealthBehaviour : MonoBehaviour
     }
 
     public event Action<(int health, int change)> OnHealthChangedCallback = delegate { };
+    public event Action<(int maxHealth, int change)> OnMaxHealthChangedCallback = delegate { };
     public event Action OnDeathCallback = delegate { };
 
-    private int _health;
+    [SerializeField, Min(0)]
+    private int _health = 10;
+    [SerializeField, Min(0)]
+    private int _maxHealth = 10;
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (_health > _maxHealth)
+            _health = _maxHealth;
+    }
+#endif
 }
