dbcf2b1985f95474071ff6746e07ba86a682fac8
Marat Davudov <md270@st-andrews.ac.uk>
2023-07-08 19:12:17 +0100

    added animations and hitboxes

--- Assets/Scripts/HeroAI/HeroStates/Running.cs
+++ Assets/Scripts/HeroAI/HeroStates/Running.cs
@@ -13,6 +13,7 @@ public class Running : BaseState
         base.Enter();
         sm = (HeroSM)this.stateMachine;
         sm.spriteRenderer.color = Color.red;
+        sm.animator.SetBool("running", true);
     }
 
     public override void UpdateLogic()
@@ -22,15 +23,20 @@ public class Running : BaseState
         {
             stateMachine.ChangeState(sm.jumpingState);
         }
+        
     }
 
     public override void UpdatePhysics()
     {
-        base.UpdatePhysics();
-        //Vector2 runningForce = 2.1f * Vector2.right * ((HeroSM)stateMachine).speed;
-
-        sm.rigidbody.MovePosition(sm.rigidbody.position + Time.deltaTime * Vector2.right);
+       
+        //Vector2 runningForce = 2f * Vector2.right * ((HeroSM)stateMachine).speed;
         //sm.rigidbody.AddForce(runningForce);
+        //sm.rigidbody.MovePosition(sm.rigidbody.position + Time.deltaTime * Vector2.right * ((HeroSM)stateMachine).speed);
+        base.UpdatePhysics();
+        Vector2 vel = sm.rigidbody.velocity;
+        vel.x =  ((HeroSM)stateMachine).speed;
+        sm.rigidbody.velocity = vel;
+        
         //Debug.Log(runningForce);
     }
 }
