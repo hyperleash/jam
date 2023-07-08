9e3aaee89b7ab5c00507dc3fa2db4e93d1268102
Celezt <59172226+Celezt@users.noreply.github.com>
2023-07-08 11:10:52 +0200

    Added ghost object where it original positioned

--- Assets/Scripts/Player/InteractBehaviour.cs
+++ Assets/Scripts/Player/InteractBehaviour.cs
@@ -11,60 +11,61 @@ public class InteractBehaviour : MonoBehaviour
     [SerializeField] private LayerMask _placeableMask;
 
     private Collider2D _dragging;
+    private GameObject _ghostObject;
     private Camera _camera;
 
     private Collider2D[] _colliding = new Collider2D[32];
     private RaycastHit2D[] _hits = new RaycastHit2D[32];
 
+    public void MoveCursor(InputAction.CallbackContext context)
+    {
+        if (_dragging is null)  // If not dragging anything.
+            return;
+
+        if (context.performed)
+            PlaceGameObject(ref _dragging);
+    }
+
     public void DragAndDrop(InputAction.CallbackContext context)
     {
         if (context.started)
         {
-            var hits = Physics2D.GetRayIntersectionAll(_camera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+            int hitCount = Physics2D.GetRayIntersectionNonAlloc(_camera.ScreenPointToRay(Mouse.current.position.ReadValue()), _hits);
 
-            if (hits.Length <= 0)
+            if (hitCount <= 0)
                 return;
 
-            foreach (var hit in hits)
+            for (int i = 0; i < hitCount; i++)
             {
-                var collider = hit.collider;
+                var collider = _hits[i].collider;
 
                 if (((1 << collider.gameObject.layer) & _interactableMask) != 0)
                 {
                     _dragging = collider;
+                    _dragging.isTrigger = true;
+
+                    if (_dragging.TryGetComponent<SpriteRenderer>(out var outSpriteRenderer) && _dragging.TryGetComponent<Collider2D>(out var outCollider2D))
+                    {
+                        _ghostObject = new GameObject("GhostObject");
+                        _ghostObject.layer = _dragging.gameObject.layer;
+                        _ghostObject.transform.position = _dragging.transform.position;
+                        _ghostObject.transform.rotation = _dragging.transform.rotation;
+                        _ghostObject.transform.localScale = _dragging.transform.localScale;
+                        CopySpriteRenderer(_ghostObject.AddComponent<SpriteRenderer>(), outSpriteRenderer);
+                    }
+
                     break;
                 }
             }
         }
         else if (context.canceled)
         {
-            if (_dragging is null)
-                return;
+            Destroy(_ghostObject);
+            PlaceGameObject(ref _dragging);
 
-            Vector2 currentPoint = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue()).xy_();
+            _dragging.isTrigger = false;
 
-            while (true)
-            {
-                int colliderCount = Physics2D.OverlapBoxNonAlloc(new Vector2(currentPoint.x, currentPoint.y + 0.05f), _dragging.bounds.size,
-                    _dragging.transform.rotation.z, _colliding, _placeableMask);
-
-                if (colliderCount <= 0) // If there is no overlap issues.
-                    break;
-
-                currentPoint.y = _colliding.First().bounds.max.y + _dragging.bounds.size.y / 2;
-                _dragging.transform.position = currentPoint;
-            }
-
-            int hitCount = Physics2D.BoxCastNonAlloc(
-                currentPoint, _dragging.bounds.size.xy(), _dragging.transform.rotation.z, 
-                Vector2.down, _hits, float.MaxValue, _placeableMask);
-
-            if (hitCount <= 0)
-                return;
-
-            currentPoint.y = _hits.First().point.y + _dragging.bounds.size.y / 2;
-            _dragging.transform.position = currentPoint;
-
+            _ghostObject = null;
             _dragging = null;
         }
     }
@@ -72,5 +73,51 @@ public class InteractBehaviour : MonoBehaviour
     private void Awake()
     {
         _camera = Camera.main;
+    }
+
+    private void PlaceGameObject(ref Collider2D collider)
+    {
+        if (collider is null)
+            return;
+
+        Vector2 currentPoint = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue()).xy_();
+
+        Debug.Log(collider.gameObject);
+        while (true)
+        {
+            int colliderCount = Physics2D.OverlapBoxNonAlloc(new Vector2(currentPoint.x, currentPoint.y + 0.05f), collider.bounds.size,
+                collider.transform.rotation.z, _colliding, _placeableMask);
+
+            if (colliderCount <= 0) // If there is no overlap issues.
+                break;
+
+            currentPoint.y = _colliding.First().bounds.max.y + collider.bounds.size.y / 2;
+            collider.transform.position = currentPoint;
+        }
+
+        int hitCount = Physics2D.BoxCastNonAlloc(
+            currentPoint, collider.bounds.size.xy(), collider.transform.rotation.z,
+            Vector2.down, _hits, float.MaxValue, _placeableMask);
+
+        if (hitCount <= 0)
+            return;
+
+        currentPoint.y = _hits.First().point.y + collider.bounds.size.y / 2;
+        collider.transform.position = currentPoint;
+    }
+
+    private void CopySpriteRenderer(SpriteRenderer copy, SpriteRenderer original)
+    {
+        copy.sprite = original.sprite;
+        copy.color = original.color;
+        copy.flipX = original.flipX;
+        copy.flipY = original.flipY;
+        copy.drawMode = original.drawMode;
+        copy.maskInteraction = original.maskInteraction;
+        copy.spriteSortPoint = original.spriteSortPoint;
+        copy.material = original.material;
+        copy.sortingLayerID = original.sortingLayerID;
+        copy.sortingOrder = original.sortingOrder;
+        copy.renderingLayerMask = original.renderingLayerMask;
     }
 }
---
+++ Assets/Scripts/Utilities/ReflectionUtility.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public static class ReflectionUtility
+{
+    public static IEnumerable<Type> GetTypesWithAttribute<T>(Assembly assembly) where T : Attribute =>
+        assembly.GetTypes().Where(t => Attribute.IsDefined(t, typeof(T)));
+
+    public static IEnumerable<Type> GetTypesWithAttribute<T>(AppDomain appDomain) where T : Attribute
+    {
+        List<Type> types = new List<Type>();
+        Assembly[] assemblies = appDomain.GetAssemblies();
+        foreach (Assembly assembly in assemblies)
+            types.AddRange(GetTypesWithAttribute<T>(assembly));
+
+        return types;
+    }
+
+    private static IEnumerable<Type> GetDerivedTypes<T>(Assembly assembly) =>
+        assembly.GetTypes().Where(t => t != typeof(T) && typeof(T).IsAssignableFrom(t));
+
+    public static IEnumerable<Type> GetDerivedTypes<T>(AppDomain appDomain)
+    {
+        List<Type> types = new List<Type>();
+        Assembly[] assemblies = appDomain.GetAssemblies();
+        foreach (Assembly assembly in assemblies)
+            types.AddRange(GetDerivedTypes<T>(assembly));
+
+        return types;
+    }
+
+    // some logic borrowed from James Newton-King, http://www.newtonsoft.com
+    public static void SetValue(this MemberInfo member, object property, object value)
+    {
+        if (member.MemberType == MemberTypes.Property)
+            ((PropertyInfo)member).SetValue(property, value, null);
+        else if (member.MemberType == MemberTypes.Field)
+            ((FieldInfo)member).SetValue(property, value);
+        else
+            throw new Exception("Property must be of type FieldInfo or PropertyInfo");
+    }
+
+    public static object GetValue(this MemberInfo member, object property)
+    {
+        if (member.MemberType == MemberTypes.Property)
+            return ((PropertyInfo)member).GetValue(property, null);
+        else if (member.MemberType == MemberTypes.Field)
+            return ((FieldInfo)member).GetValue(property);
+        else
+            throw new Exception("Property must be of type FieldInfo or PropertyInfo");
+    }
+
+    public static Type GetType(this MemberInfo member) => member.MemberType switch
+    {
+        MemberTypes.Field => ((FieldInfo)member).FieldType,
+        MemberTypes.Property => ((PropertyInfo)member).PropertyType,
+        MemberTypes.Event => ((EventInfo)member).EventHandlerType,
+        _ => throw new ArgumentException("MemberInfo must be if type FieldInfo, PropertyInfo or EventInfo", "member"),
+    };
+}
