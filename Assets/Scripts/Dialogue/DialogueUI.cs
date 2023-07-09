d0793143ea2ee1d0bb13273faebf3b0a4e46ad77
Celezt <59172226+Celezt@users.noreply.github.com>
2023-07-09 10:32:20 +0200

    Updated dialogue with portrait support

--- Assets/Scripts/Dialogue/DialogueUI.cs
+++ Assets/Scripts/Dialogue/DialogueUI.cs
@@ -2,8 +2,10 @@ using Celezt.DialogueSystem;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class DialogueUI : MonoBehaviour
 {
@@ -11,7 +13,11 @@ public class DialogueUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _rightTextGUI;
     [SerializeField] private TextMeshProUGUI _leftActorGUI;
     [SerializeField] private TextMeshProUGUI _rightActorGUI;
+    [SerializeField] private Image _leftPortrait;
+    [SerializeField] private Image _rightPortrait;
     [SerializeField] private PlayableDirector _playableDirector;
+
+    [SerializeField] private SerializableDictionary<string, Sprite> _portraits;
 
     public void OnProcessClip(DialogueSystemBinder.Callback callback)
     {
@@ -22,12 +28,24 @@ public class DialogueUI : MonoBehaviour
                 _leftActorGUI.text = asset.Actor;
                 _leftTextGUI.text = asset.Text;
                 _leftTextGUI.maxVisibleCharacters = Mathf.CeilToInt(_leftTextGUI.textInfo.characterCount * callback.Percentage);
+
+                if (_portraits.ContainsKey(asset.Actor))
+                {
+                    _leftPortrait.enabled = true;
+                    _leftPortrait.sprite = _portraits[asset.Actor];
+                }
             }
             else if (callback.Index == 1)
             {
                 _rightActorGUI.text = asset.Actor;
                 _rightTextGUI.text = asset.Text;
                 _rightTextGUI.maxVisibleCharacters = Mathf.CeilToInt(_rightTextGUI.textInfo.characterCount * callback.Percentage);
+
+                if (_portraits.ContainsKey(asset.Actor))
+                {
+                    _rightPortrait.enabled = true;
+                    _rightPortrait.sprite = _portraits[asset.Actor];
+                }
             }
         }
     }
@@ -38,11 +56,13 @@ public class DialogueUI : MonoBehaviour
         {
             _leftTextGUI.text = null;
             _leftActorGUI.text = null;
+            _leftPortrait.enabled = false;
         }
         else if (callback.Index == 1)   // Right.
         {
             _rightTextGUI.text = null;
             _rightActorGUI.text = null;
+            _rightPortrait.enabled = false;
         }
     }
 
@@ -52,11 +72,13 @@ public class DialogueUI : MonoBehaviour
         {
             _leftTextGUI.text = null;
             _leftActorGUI.text = null;
+            _leftPortrait.enabled = false;
         }
         else if (callback.Index == 1)   // Right.
         {
             _rightTextGUI.text = null;
             _rightActorGUI.text = null;
+            _rightPortrait.enabled = false;
         }
     }
 }
