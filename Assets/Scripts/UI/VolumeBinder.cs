7582cb5bd5b672c8f757513f90914ab26dee584a
Celezt <59172226+Celezt@users.noreply.github.com>
2023-07-12 18:29:51 +0200

    Updated VolumeBinder

--- Assets/Scripts/UI/VolumeBinder.cs
+++ Assets/Scripts/UI/VolumeBinder.cs
@@ -3,20 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class VolumeBinder : MonoBehaviour
 {
     [SerializeField] private AudioMixer _audioMixer;
 
-    [SerializeField] private UnityEvent _onEnable;
+    [SerializeField] private string _parameterName;
+    [SerializeField] private MonoBehaviour _bindReference;
 
     public void SetLevel(float value)
     {
-        _audioMixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+        if (string.IsNullOrWhiteSpace(_parameterName))
+            return;
+
+        _audioMixer.SetFloat(_parameterName, Mathf.Log10(value) * 20);
     }
 
     private void OnEnable()
     {
-        _onEnable.Invoke();
+        if (_bindReference == null)
+            return;
+
+        switch (_bindReference)
+        {
+            case Slider slider:
+                SetLevel(slider.value);
+                slider.onValueChanged.AddListener(SetLevel);
+                break;
+            default:
+                Debug.LogWarning($"{_bindReference.GetType()} is not supported.");
+                break;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_bindReference == null)
+            return;
+
+        switch (_bindReference)
+        {
+            case Slider slider:
+                slider.onValueChanged.RemoveListener(SetLevel);
+                break;
+            default:
+                Debug.LogWarning($"{_bindReference.GetType()} is not supported.");
+                break;
+        }
     }
 }
